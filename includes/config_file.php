6ba5291e41db434e0d87c86654dd6f342a28b3dd
ajaykant00 <ajaykant900@gmail.com>
2015-01-06 17:32:23 +0530

    Source of Microwave Lab has been added

---
+++ includes/config_file.php
@@ -0,0 +1,23 @@
+<?php
+set_time_limit(0);
+define('DB_HOST','localhost');
+define('DB_USER','sandeep4_user');
+define('DB_PASSWORD','p@ssword12345');
+define('DB_DATABASE','sandeep4_virtual');
+define('DB_TYPE','mysql');
+define('CONNECTION_TYPE','normal'); //persist
+
+define('USE_MAILER','SENDMAIL');
+define('SMTP_SERVER','smtp.gmail.com');
+define('SMTP_USERNAME','developer.cool@gmail.com');
+define('SMTP_PASSWORD','ns7679');
+define('SMTP_SECURITY','tls');
+define('SMTP_PORT',465);
+
+define('TABLE_PREFIX','');
+define('SESSION_PREFIX','IEE_');
+define('SERVER_URL','http://websupplements.net');
+define('APP_FOL','/projects/virtual_labs/');
+define('DB_SESSION',false);
+define('APP_NAME','Virtual Labs');
+?>
