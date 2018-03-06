6bb2afd81c9d57e6f96f91f94e5e8fe14c37371f
root <root@localhost.localdomain>
2018-03-06 11:34:27 +0530

    Updated the source and fixed the errors

--- scripts/db_config.sh
+++ scripts/db_config.sh
@@ -3,10 +3,11 @@
 # To avoid prompt for user credentials
 export DEBIAN_FRONTEND=noninteractive
 apt-get -q -y install mysql-server
+apt-get php5-mysql -y
 # mysql -u root -Bse "create database YOUR_DATABASE_NAME;"
-mysql -u root -Bse "create database sandeep4_virtual;"
+mysql -u root -Bse "create database websupp2_virtual;"
 cd ../src/database
 # Replace "emt" with your database name
 # Replace "emt.sql" file with your file
 # Note: If you have more than one .sql file repeat below command w.r.t to database and file name
-mysql -u root sandeep4_virtual<sandeep4_virtual.sql
+mysql -u root websupp2_virtual<websupp2_virtual.sql
